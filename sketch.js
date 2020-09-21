f7c1455666a352a1c812a2c8982f33229d2c4018
vedansh06 <67477921+vedansh06@users.noreply.github.com>
2020-09-21 20:13:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var ground1;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+  ground1 = new Ground(700,350,700,20);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground1.display();
+  drawSprites();
+ 
+}
+
+
+
